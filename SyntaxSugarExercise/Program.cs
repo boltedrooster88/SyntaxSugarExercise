9bf784f6b4e79d64f5bf8946ecd2a8c46b76479d
Robert Lane <guidelane88@gmail.com>
2021-02-24 20:07:39 -0600

    first save

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+            int answer = 4;
+            var repsonse = (answer < 9) ? $"{answer}is less than nine" : $"{answer} greater than or equal to nine"; //Ternary Operator
+            Console.WriteLine(repsonse);
+            
+
+
+
+
+        }
+    }
+}
